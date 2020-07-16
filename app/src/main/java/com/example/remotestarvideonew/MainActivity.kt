ee2cb9aa7a7f53ab65823a7df499752a8a783d8f
Ashish Gupta <gupta.ashish@lattukids.com>
2020-07-16 22:58:23 +0530

    integrated shimmer and eror view in main acitivity

--- app/src/main/java/com/example/remotestarvideonew/MainActivity.kt
+++ app/src/main/java/com/example/remotestarvideonew/MainActivity.kt
@@ -2,18 +2,21 @@ package com.example.remotestarvideonew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.facebook.shimmer.ShimmerFrameLayout
 import java.util.ArrayList
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , SwipeRefreshLayout.OnRefreshListener{
 
-//    private var viewModel: ViewModel? = null
+    private var viewModel: ViewModel? = null
     private var recyclerView: RecyclerView? = null
     private var allVideos: ArrayList<Video>? = ArrayList()
     private var shimmerFrameLayout: ShimmerFrameLayout? = null
@@ -44,9 +47,57 @@ class MainActivity : AppCompatActivity() {
         recyclerView!!.layoutManager = LinearLayoutManager(this)
         noteAdapter = ContactsRecyclerViewAdapter(allVideos!!,this )
         recyclerView!!.adapter = noteAdapter
-//        intialiseViewModel()
+        intialiseViewModel()
 
 
+    }
+
+    fun intialiseViewModel(){
+
+        viewModel = ViewModelProviders.of(this).get(ViewModel::class.java)
+
+        viewModel!!.apiStatus.observe(this, Observer { aBoolean ->
+            if (aBoolean == false) {
+                apiFailed()
+            }
+        })
+
+        viewModel!!.videos.observe(this, Observer { videos ->
+            Handler().postDelayed({
+                dataRecived(videos.results!!)
+            },1000)
+        })
+
+    }
+
+    override fun onRefresh() {
+        fetchAgain("no-cache")
+    }
+
+    fun apiFailed(){
+        shimmerFrameLayout!!.stopShimmerAnimation()
+        shimmerFrameLayout!!.visibility = View.GONE
+        swipeRefreshLayout!!.setRefreshing(false);
+        swipeRefreshLayout!!.visibility = View.GONE
+        errorOuterLayout!!.visibility = View.VISIBLE
+    }
+
+    fun dataRecived(result:ArrayList<Video>){
+
+        shimmerFrameLayout!!.stopShimmerAnimation()
+        shimmerFrameLayout!!.visibility = View.GONE
+        swipeRefreshLayout!!.setRefreshing(false);
+        swipeRefreshLayout!!.visibility = View.VISIBLE
+        noteAdapter!!.setData(result)
+
+    }
+
+    fun fetchAgain(cahceControl:String?){
+        swipeRefreshLayout!!.visibility = View.GONE
+        shimmerFrameLayout!!.visibility = View.VISIBLE
+        shimmerFrameLayout!!.startShimmerAnimation()
+        errorOuterLayout!!.visibility = View.GONE
+        viewModel!!.fetchVideos(cahceControl)
     }
 
 }
